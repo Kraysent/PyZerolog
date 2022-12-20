a9b95e2f7fdb6cd0831d0ef2ca8c8b25f7ba07ad
Artyom Zaporozhets <kraysent@gmail.com>
2022-12-20 11:49:24 +0300

    use pyproject.toml instead of setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="pyzerolog",
-    version="0.3.0",
-    author="Kraysent",
-    description="Package that makes typed logging convinient.",
-    license="MIT License",
-    long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
-    packages=["zlog"],
-)
+setup()
